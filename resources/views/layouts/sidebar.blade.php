78b28db59849ab6d4adc9f368e754a25897f00e0
Paweł Puchowicz <pawelpuchowicz17@gmail.com>
2020-11-24 20:04:06 +0100

    Logo
    AdmiLTE->PlatformaEDU

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <img src={{asset('dist/img/AdminLTELogo.png')}} alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src={{asset('dist/img/platformaedulogo.png')}} class="brand-image">
+        <img src="{{asset('dist/img/platformaedu.png')}}">
+{{--        <span class="brand-text font-weight-light">Platforma EDU</span>--}}
     </a>
 
 
