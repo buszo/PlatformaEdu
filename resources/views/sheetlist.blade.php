bf4abff3c0ca4c8f735ce2f22f2a19662b2bd8d1
fren733 <filipren6@gmail.com>
2020-12-05 23:39:27 +0100

    wysiwyg v1.7.1

--- resources/views/sheetlist.blade.php
+++ resources/views/sheetlist.blade.php
@@ -13,18 +13,25 @@
         <div class="row">
             <div class="col-md-3 order-md-2">
                 <div class="card">
-                    <div class="card-header"></div>
                     <div class="card-header">
-                        <a href="#">Nowy arkusz</a>
-                    </div>
+                    <h5 class="pt-2" style="display:inline-block;">Nowy arkusz</h5>
+                        <div class="add-sheet">
+                            <a id="submit" role="button" href="{{route('editor')}}">
+                                <i class="fas fa-plus-square"></i>
+                            </a>
+                        </div></div>
                     <div class="card-body">
                     <form>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Tytuł">
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Opis">
                         </div>
+                        <div class="form-group row mr-1 ml-1">
+                                        od<input itype="text" class="form-control col">do
+                                        <input class="form-control col" type="text">
+                                    </div>
                         <button class="btn btn-primary" type="submit">Szukaj</button>
                     </form>
                     </div>
@@ -32,84 +39,134 @@
                 </div>
             </div>
             <div class="col-md-9 order-md-1">
-                <div class="card">
-                    <div class="card-header pt-3 pb-3">
+                <div class="card card-outline card-secondary">
+                    <div class="card-header">
                         <h3 style="display:inline-block;">Dodane arkusze</h3>
-                        <div class="add-sheet">
-                            <a id="submit" role="button" href="{{route('editor')}}">
-                                <i class="fas fa-plus-square"></i>
-                            </a>
-                        </div>
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h5 class="card-header">Featured</h5>
+                            <div class="col-md-6">
+                                <div class="card" style="height:230px;">
+                                    <h5 class="card-header">Test tytuł</h5>
                                     <div class="card-body">
-                                        <h5 class="card-title">Special title treatment</h5>
-                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content. With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer btn-group mt-4" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="card">
+                            <div class="col-md-6">
+                                <div class="card" style="height:230px;">
                                     <h5 class="card-header">Featured</h5>
                                     <div class="card-body">
                                         <h5 class="card-title">Special title treatment</h5>
                                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h5 class="card-header">Featured</h5>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Special title treatment</h5>
-                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <div class="card-footer btn-group mt-4" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class="col-md-4">
+                            
+                            <div class="col-md-6">
                                 <div class="card">
                                     <h5 class="card-header">Featured</h5>
                                     <div class="card-body">
                                         <h5 class="card-title">Special title treatment</h5>
                                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-6">
                                 <div class="card">
                                     <h5 class="card-header">Featured</h5>
                                     <div class="card-body">
                                         <h5 class="card-title">Special title treatment</h5>
                                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h5 class="card-header">Featured</h5>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Special title treatment</h5>
-                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                    
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <h5 class="card-header">Featured</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Special title treatment</h5>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <h5 class="card-header">Featured</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Special title treatment</h5>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <h5 class="card-header">Featured</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Special title treatment</h5>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <h5 class="card-header">Featured</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Special title treatment</h5>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer btn-group" style="width:100%; padding:0;">
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-info-circle"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-edit"></i></a>
+                                            <a href="#" style="color:#757575" class="btn btn-light"><i class="fas fa-trash-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-footer">
 
